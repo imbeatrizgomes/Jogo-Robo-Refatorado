29cbc0721e645b579d7bfcb459a1d15e614543e7
Beatriz <anabeatrizsantos3830@gmail.com>
2025-11-29 13:26:51 -0300

    Update RoboCompeticaoInteligente.java

--- jogo_robo_versao_refatorada/src/application/RoboCompeticaoInteligente.java
+++ jogo_robo_versao_refatorada/src/application/RoboCompeticaoInteligente.java
@@ -22,9 +22,9 @@ public class RoboCompeticaoInteligente {
         int comHorizontal; 
         int comVertical;
         do {
-        	System.out.print("Digite a posição horizontal X da comida [0–" + (mundo.getTAMANHO_TABULEIRO() - 1) + "]: ");
+        	System.out.print("Digite a posição horizontal X da comida [0–" + (mundo.getTamanhoTabuleiro() - 1) + "]: ");
             comHorizontal = sc.nextInt();
-            System.out.print("Digite a posição vertical Y da comida [0–" + (mundo.getTAMANHO_TABULEIRO() - 1) + "]: ");
+            System.out.print("Digite a posição vertical Y da comida [0–" + (mundo.getTamanhoTabuleiro() - 1) + "]: ");
             comVertical = sc.nextInt();
         } while (!mundo.posicaoValida(comHorizontal, comVertical));
 
@@ -65,6 +65,7 @@ public class RoboCompeticaoInteligente {
         sc.close();
     }
 }
+
 
 
 
