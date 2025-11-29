83e45bd6051d429c3c27ce850c14c5a188a37af5
Beatriz <anabeatrizsantos3830@gmail.com>
2025-11-29 14:33:41 -0300

    Update MundoRobo.java

--- jogo_robo_versao_refatorada/src/entities/MundoRobo.java
+++ jogo_robo_versao_refatorada/src/entities/MundoRobo.java
@@ -4,10 +4,10 @@ public class MundoRobo {
     private static final int TAMANHO_TABULEIRO = DimensaoTabuleiro.SIZE;
     private int comidaX;
     private int comidaY;
-    private Obstaculo[][] obstaculos = new Obstaculo[TAMANHO_TABULEIRO ][TAMANHO_TABULEIRO ];
+    private Obstaculo[][] obstaculos = new Obstaculo[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
 
     public boolean posicaoValida(int x, int y) {
-        return x >= 0 && x < TAMANHO_TABULEIRO && y >= 0 && y < TAMANHO_TABULEIRO ;
+        return x >= 0 && x < TAMANHO_TABULEIRO && y >= 0 && y < TAMANHO_TABULEIRO;
     }
 
     public void addObstaculo(Obstaculo obstaculo) {
@@ -34,28 +34,9 @@ public class MundoRobo {
     public String gerarVisualizacao(Robo robo) {
         StringBuilder sb = new StringBuilder("\n");
 
-        for (int y = TAMANHO_TABULEIRO  - 1; y >= 0; y--) {
-            for (int x = 0; x < TAMANHO_TABULEIRO ; x++) {
-
-                boolean roboAqui = robo.isAtivo() &&
-                                   robo.getX() == x &&
-                                   robo.getY() == y;
-
-                if (roboAqui) {
-                    sb.append("[").append(robo.getCor().charAt(0)).append("]");
-                }
-                else if (comidaX == x && comidaY == y) {
-                    sb.append("[X]");
-                }
-                else if (obstaculos[x][y] instanceof Bomba) {
-                    sb.append("[B]");
-                }
-                else if (obstaculos[x][y] instanceof Rocha) {
-                    sb.append("[R]");
-                }
-                else {
-                    sb.append("[·]");
-                }
+        for (int y = TAMANHO_TABULEIRO - 1; y >= 0; y--) {
+            for (int x = 0; x < TAMANHO_TABULEIRO; x++) {
+                sb.append(addSimboloUmRobo(x, y, robo));
             }
             sb.append("\n");
         }
@@ -63,67 +44,83 @@ public class MundoRobo {
         sb.append("\n");
         return sb.toString();
     }
-    
+
     public String gerarVisualizacao(Robo robo1, Robo robo2) {
         StringBuilder sb = new StringBuilder("\n");
 
-        for (int y = TAMANHO_TABULEIRO  - 1; y >= 0; y--) {
-            for (int x = 0; x < TAMANHO_TABULEIRO ; x++) {
-
-                boolean robo1Aqui = robo1.isAtivo() &&
-                                    robo1.getX() == x &&
-                                    robo1.getY() == y;
-
-                boolean robo2Aqui = robo2.isAtivo() &&
-                                    robo2.getX() == x &&
-                                    robo2.getY() == y;
-
-                if (robo1Aqui && robo2Aqui) {
-                    sb.append("[")
-                      .append(robo1.getCor().charAt(0))
-                      .append(robo2.getCor().charAt(0))
-                      .append("]");
-                }
-                else if (robo1Aqui) {
-                    sb.append("[").append(robo1.getCor().charAt(0)).append("]");
-                }
-                else if (robo2Aqui) {
-                    sb.append("[").append(robo2.getCor().charAt(0)).append("]");
-                }
-                else if (comidaX == x && comidaY == y) {
-                    sb.append("[X]");
-                }
-                else if (obstaculos[x][y] instanceof Bomba) {
-                    sb.append("[B]");
-                }
-                else if (obstaculos[x][y] instanceof Rocha) {
-                    sb.append("[R]");
-                }
-                else {
-                    sb.append("[·]");
-                }
+        for (int y = TAMANHO_TABULEIRO - 1; y >= 0; y--) {
+            for (int x = 0; x < TAMANHO_TABULEIRO; x++) {
+                sb.append(addSimboloDoisRobos(x, y, robo1, robo2));
             }
             sb.append("\n");
         }
-        
+
         sb.append("\n");
         return sb.toString();
     }
 
-    public int getComidaX() { 
-    	return comidaX; 
+    private String addSimboloUmRobo(int x, int y, Robo robo) {
+        boolean roboAqui = robo != null && robo.isAtivo() && robo.getX() == x && robo.getY() == y;
+
+        if (roboAqui) {
+            return "[" + robo.getCor().charAt(0) + "]";
+        }
+        if (comidaX == x && comidaY == y) {
+            return "[X]";
+        }
+        Obstaculo ob = obstaculos[x][y];
+        if (ob instanceof Bomba) {
+            return "[B]";
+        }
+        if (ob instanceof Rocha) {
+            return "[R]";
+        }
+        return "[·]";
     }
-    public void setComidaX(int comidaX) { 
-    	this.comidaX = comidaX; 
+
+    private String addSimboloDoisRobos(int x, int y, Robo r1, Robo r2) {
+        boolean r1Aqui = r1 != null && r1.isAtivo() && r1.getX() == x && r1.getY() == y;
+        boolean r2Aqui = r2 != null && r2.isAtivo() && r2.getX() == x && r2.getY() == y;
+
+        if (r1Aqui && r2Aqui) {
+            return "[" + r1.getCor().charAt(0) + r2.getCor().charAt(0) + "]";
+        }
+        if (r1Aqui) {
+            return "[" + r1.getCor().charAt(0) + "]";
+        }
+        if (r2Aqui) {
+            return "[" + r2.getCor().charAt(0) + "]";
+        }
+        if (comidaX == x && comidaY == y) {
+            return "[X]";
+        }
+        Obstaculo ob = obstaculos[x][y];
+        if (ob instanceof Bomba) {
+            return "[B]";
+        }
+        if (ob instanceof Rocha) {
+            return "[R]";
+        }
+        return "[·]";
     }
-    public int getComidaY() { 
-    	return comidaY; 
+
+    public int getComidaX() {
+        return comidaX;
     }
-    public void setComidaY(int comidaY) { 
-    	this.comidaY = comidaY;
+
+    public void setComidaX(int comidaX) {
+        this.comidaX = comidaX;
     }
+
+    public int getComidaY() {
+        return comidaY;
+    }
+
+    public void setComidaY(int comidaY) {
+        this.comidaY = comidaY;
+    }
+
     public int getTamanhoTabuleiro() {
         return TAMANHO_TABULEIRO;
     }
 }
-
